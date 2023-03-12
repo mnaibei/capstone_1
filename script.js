4ca8d9abb74eec0b08d5cce3cba145e99a38e48a
mnaibei <muchajulius@gmail.com>
2023-03-12 18:44:42 +0300

    Made artist section dynamic

--- script.js
+++ script.js
@@ -11,3 +11,72 @@ document.querySelectorAll('.mobile').forEach((n) => n.addEventListener('click', 
   hamburger.classList.remove('active');
   mobile.classList.remove('active');
 }));
+
+// artist section dynamic 
+const data = [
+  {
+    image:'images/shinski.webp',
+    artist: 'Nyashinski',
+    type: 'Rapper',
+    rating:'⭐️⭐️⭐️⭐️⭐️',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium beatae quibusdam, hic saepe minima dolores labore aliquid nesciunt dolor!',
+
+  },
+  {
+    image:'images/femi-one.webp',
+    artist: 'Femi One',
+    type: 'Rapper',
+    rating:'⭐️⭐️⭐️⭐️⭐️',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium beatae quibusdam, hic saepe minima dolores labore aliquid nesciunt dolor!',
+  },
+  {
+    image:'images/femi-one.webp',
+    artist: 'Femi One',
+    type: 'Rapper',
+    rating:'⭐️⭐️⭐️⭐️⭐️',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium beatae quibusdam, hic saepe minima dolores labore aliquid nesciunt dolor!',
+  },
+  {
+    image:'images/femi-one.webp',
+    artist: 'Femi One',
+    type: 'Rapper',
+    rating:'⭐️⭐️⭐️⭐️⭐️',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium beatae quibusdam, hic saepe minima dolores labore aliquid nesciunt dolor!',
+  },
+  {
+    image:'images/femi-one.webp',
+    artist: 'Femi One',
+    type: 'Rapper',
+    rating:'⭐️⭐️⭐️⭐️⭐️',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium beatae quibusdam, hic saepe minima dolores labore aliquid nesciunt dolor!',
+  },
+  {
+    image:'images/femi-one.webp',
+    artist: 'Femi One',
+    type: 'Rapper',
+    rating:'⭐️⭐️⭐️⭐️⭐️',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam praesentium beatae quibusdam, hic saepe minima dolores labore aliquid nesciunt dolor!',
+  },
+];
+
+//calling and appending to DOM
+const artistSection = document.querySelector('.test-artists');
+data.forEach((content) => {
+  const item = document.createElement('artists');
+  item.classList.add('d-flex');
+
+  item.innerHTML = `
+  <div class="artists d-flex my-3 align-items-center">
+  <img class="pics mx-2" src="${content.image}" alt="${content.artist}">
+  <div class="artist_info">
+    <h3 class="artist_name my-0 fw-bold py-2">${content.artist}</h3>
+    <p class="artist_type my-0 py-1">${content.type}</p>
+    <p class="artist_rating my-0 py-1">${content.rating}</p>
+    <hr align="center" class="my-0" width="10%" size="4">
+    <p class="artist_desc my-0 py-1">${content.desc}.</p>
+  </div>
+</div>
+  `;
+
+  artistSection.appendChild(item);
+});
